7b5a07955655b1479907d6b7c74e6f2709fe54fd
tclindner <tclindner@users.noreply.github.com>
2020-05-02 13:46:55 -0500

    Create runCommand.js

---
+++ src/runCommand.js
@@ -0,0 +1,18 @@
+const {execFile} = require('child_process');
+
+// eslint-disable-next-line require-await
+const runCommand = async (filePath, args) => {
+  return new Promise((resolve, reject) => {
+    execFile(filePath, args, (error, stdout) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(stdout);
+    });
+  });
+};
+
+module.exports = {
+  runCommand,
+};
